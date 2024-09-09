bbf880c056d647309df4f060b4bc2e0bed9519dd
Noah <Nphilli7@students.tulsatech.edu>
2024-09-09 14:48:20 -0500

    almost done with part 1

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by PM Student on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
